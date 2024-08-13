2bce9c617b43efb5e79c97dbbb1e64677ef281af
ian pires <ianbr23@gmail.com>
2024-08-13 09:19:18 -0300

    Project created

---
+++ src/test/java/com/ian/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ian.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
