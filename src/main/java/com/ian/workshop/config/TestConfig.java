a288741733a1779304e0759e377b89a540c1f2b0
ian pires <ianbr23@gmail.com>
2024-08-15 11:11:43 -0300

    Payment, one-to-one association

--- src/main/java/com/ian/workshop/config/TestConfig.java
+++ src/main/java/com/ian/workshop/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ian.workshop.entities.Category;
 import com.ian.workshop.entities.Order;
 import com.ian.workshop.entities.OrderItem;
+import com.ian.workshop.entities.Payment;
 import com.ian.workshop.entities.Product;
 import com.ian.workshop.entities.User;
 import com.ian.workshop.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
